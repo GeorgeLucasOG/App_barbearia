9c0935e48087354d7a6edf2f758835fa19f63948
GeorgeLucasOG <lucas@excelinformatica.com>
2024-02-10 06:32:04 -0300

    feat: add sign in and sign out

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,13 +3,14 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import { Adapter } from "next-auth/adapters";
+import { ReactNode } from "react";
 
 const handler = NextAuth({
     adapter: PrismaAdapter(db) as Adapter,
     providers:[
         GoogleProvider({
-            clientId: "process.env.GOOGLE_CLIENT_ID" as string,
-            clientSecret: "process.env.GOOGLE_CLIENT_SECRET" as string,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
 });
