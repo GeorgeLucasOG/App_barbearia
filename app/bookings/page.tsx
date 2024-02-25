9e88feb4dfd036d4c8685f0cf5f1cff431e3c370
GeorgeLucasOG <lucas@excelinformatica.com>
2024-02-25 00:43:22 -0300

    feat: ensure booking can be canceled

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,11 +1,10 @@
 import Header from "../_components/header";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
-import { signIn } from "next-auth/react";
 import { redirect } from "next/navigation";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
-import { isFuture, isPast } from "date-fns";
+
 
 const BookingsPage = async () => {
 
@@ -50,9 +49,11 @@ const BookingsPage = async () => {
         <div className="px-5 py-6">
             <h1 className="text-xl font-bold">Agendamentos</h1>
 
-            <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">
-                Confirmados
-            </h2>
+            {confirmedBookings.length > 0  && (
+                <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">
+                    Confirmados
+                </h2>
+            )}
 
             <div className="flex flex-col gap-3">
                 {confirmedBookings.map((booking) => (
